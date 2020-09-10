13f819042a371fbb3c5b2b604db3b21bdc3209b6
Arthur De Meyer <arthur.demeyer75@gmail.com>
2020-09-10 09:35:21 +0200

    almost done styling the Shopping Crat page - needs editing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :mycvs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'mycvs#index'
+  match 'download', to: 'mycvs#download', as: 'download', via: :get
 
   resources :pages
 end
